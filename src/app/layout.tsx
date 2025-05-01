7da57d79ab6f693e57a5c18d7e1b0bd7af13ebc6
Andrey <108063187+Minaht007@users.noreply.github.com>
2025-05-01 10:41:55 +0300

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import "./globals.css";
 
 import Header from "../components/header"
+import Footer from "../components/footer"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -39,6 +40,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
