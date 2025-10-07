fb34a8f0549e7784fa707102220dea8944a1dfe0
Andrey <108063187+Minaht007@users.noreply.github.com>
2025-10-07 20:24:00 +0300

    new analytics connect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,49 +6,67 @@ import "./globals.css";
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
 
+import Script from "next/script";
+import AnalyticsTracker from "../components/AnalyticsTracker";
+
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 const interFont = Inter({
-	subsets: ["latin"],
-	variable: "--font-inter",
+  subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
-	title: "Auto Tools",
-	description:
+  title: "Auto Tools",
+  description:
     "Онлайн-магазин автозапчастей в Україні. Підбір деталей за маркою, моделлю або VIN-кодом. Оригінальні та якісні аналоги з гарантією. Швидка доставка по всій Україні.",
-    keywords:   
+  keywords:
     "автозапчастини, інтернет магазин автозапчастин, купити автозапчастини, запчастини для авто, автозапчастини Україна, автозапчастини онлайн, автозапчастини по VIN, підбір автозапчастин, оригінальні автозапчастини, аналоги запчастин, доставка автозапчастин, автозапчастини Київ, автозапчастини Львів, автозапчастини Харків, автозапчастини Одеса, магазин автозапчастин Україна" +
-    
     "автозапчасти, интернет магазин автозапчастей, купить автозапчасти, запчасти для авто, автозапчасти Украина, автозапчасти онлайн, подбор автозапчастей, оригинальные автозапчасти, аналоги запчастей, доставка автозапчастей, автозапчасти Киев, автозапчасти Львов, автозапчасти Харьков, автозапчасти Одесса, магазин автозапчастей Украина",
-  
+
   authors: [{ name: "DEV-HAWKS" }],
   creator: "DEV-HAWKS TEAM",
   robots: "index, follow",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} ${interFont.variable} antialiased layout`}
-			>
-				<Header />
-				{children}
-				<Footer />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${interFont.variable} antialiased layout`}
+      >
+        <Header />
+        {children}
+        <Footer />
+
+        <AnalyticsTracker />
+
+        {/* Google Analytics скрипты */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-Q2DR93620Q`}
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', 'G-Q2DR93620Q');
+  `}
+        </Script>
+      </body>
+    </html>
+  );
 }
